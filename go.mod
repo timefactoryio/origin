3dae46d4a72ac95e77ccc21061721c0cb8120989
zachklingbeil <zach@zachklingbeil.com>
2026-07-26 10:16:18 -0700

    commit

--- go.mod
+++ go.mod
@@ -1,15 +1,15 @@
 module github.com/timefactoryio/origin
 
-go 1.26.4
+go 1.26.5
 
 require github.com/timefactoryio/pathless v0.10.10
 
-// replace github.com/timefactoryio/pathless => ../pathless
+replace github.com/timefactoryio/pathless => ../pathless
 
 require (
 	github.com/alecthomas/chroma/v2 v2.27.0 // indirect
-	github.com/dlclark/regexp2/v2 v2.2.2 // indirect
+	github.com/dlclark/regexp2/v2 v2.5.2 // indirect
 	github.com/timefactoryio/markdown v0.0.5 // indirect
-	github.com/yuin/goldmark v1.8.2 // indirect
+	github.com/yuin/goldmark v1.8.4 // indirect
 	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"os"
-
 	"github.com/timefactoryio/pathless"
 )
 
-func main() {
-	p := pathless.NewPathless(os.Getenv("ORIGIN"), os.Getenv("CIRCUIT"))
-	p.Home(os.Getenv("LOGO"), os.Getenv("TITLE"))
-	p.Text(os.Getenv("TEXT"))
-	p.Slides(os.Getenv("SLIDES"))
-	p.Panels(p.Keyboard())
-	p.Serve()
-}
-
 // func main() {
-// 	p := pathless.NewPathless()
-// 	p.Home("https://zero.s3.timefactory.io/timefactory.svg", "the point of origin")
-// 	p.Slides("https://zero.s3.timefactory.io/slides")
-// 	p.Text("https://raw.githubusercontent.com/timefactoryio/pathless/refs/heads/main/mechanics.md")
-// 	p.Panels(p.Keyboard())
+// 	p := pathless.NewPathless(os.Getenv("ORIGIN"), os.Getenv("CIRCUIT"))
+// 	p.Home(os.Getenv("LOGO"), os.Getenv("TITLE"))
+// 	p.Text(os.Getenv("TEXT"))
+// 	p.Slides(os.Getenv("SLIDES"))
 // 	p.Serve()
 // }
+
+func main() {
+	p := pathless.NewPathless()
+	p.Home("https://zero.s3.timefactory.io/timefactory.svg", "the point of origin")
+	p.Text("../pathless/README.md")
+	p.Text("../pathless/mechanics.md")
+	p.Text("../pathless/zero/README.md")
+	p.Text("../pathless/fx/README.md")
+	p.Text("../pathless/one/README.md")
+	p.Keyboard()
+	p.Serve()
+}
